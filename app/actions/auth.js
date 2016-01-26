52ea24200f4b5ec80af32acea553842fd007839b
Alexander Dukhovnyak <shanhaichik@gmail.com>
2016-01-26 03:38:20 +0300

    auth lint

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -13,56 +13,52 @@ const baseUrl = 'http://localhost:7000/api';
 
 // TODO: решить вопрос с переходом при авторизации
 export function login(username, password, router) {
-  return async (dispatch) => {
-    try {
-          const { data: { success, message, token } } = await axios.post(`${baseUrl}/login`, {
-            username,
-            password
-          });
+    return async (dispatch) => {
+        try {
+            const { data: { success, message, token } } = await axios.post(`${baseUrl}/login`, {
+                username,
+                password
+            });
 
-          if (success === true) {
+            if (success === true) {
                 // Сохраняем в Session storage
-                if(token !== 'undefined') {
-                    sessionStorage.setItem('token',token);
+                if (token !== 'undefined') {
+                    sessionStorage.setItem('token', token);
                 }
 
-                dispatch({ type: LOGIN_SUCCESS, token: token });
-                //Временная переадресация при логине
+                dispatch({ type: LOGIN_SUCCESS, token });
+                // Временная переадресация при логине
                 const { query } = router.state.location;
                 const redirectTo = (query && query.redirectTo) ? query.redirectTo : '/home';
                 router.transitionTo(redirectTo);
+            } else {
+                dispatch({ type: LOGIN_FAILURE, error: message });
             }
-            else{
-                dispatch({ type: LOGIN_FAILURE, error:message});
-            }
-
+        } catch (e) {
+            dispatch({ type: LOGIN_FAILURE, error: e.data.message });
         }
-        catch (e) {
-            dispatch({ type: LOGIN_FAILURE, error: e.data.message});
-        }
-
-    }
+    };
 }
 
 
 export function logout(router) {
-    return async(dispatch) => {
-
+    return async (dispatch) => {
         try {
-            const { data:{ success }} = await axios.get(`${baseUrl}/logout`,{
-                headers:{
-                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            const { data: { success } } = await axios.get(`${baseUrl}/logout`, {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem('token')}`
                 }
             });
 
-            sessionStorage.removeItem('token');
+            if (success === true && !!sessionStorage.removeItem('token')) {
+                sessionStorage.removeItem('token');
+            }
 
-            dispatch({ type: LOGOUT_SUCCESS});
+            dispatch({ type: LOGOUT_SUCCESS });
 
             router.transitionTo(...redirectBackAfter('/login', router.state));
+        } catch (e) {
+            dispatch({ type: LOGOUT_FAILURE });
         }
-        catch (e) {
-            dispatch({ type: LOGOUT_FAILURE});
-        }
-    }
+    };
 }
